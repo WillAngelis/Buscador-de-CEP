95287f503fae8b654b5319d9d3d6d3484ef2c732
William Angelis <willangelis@gmail.com>
2022-01-10 01:12:48 -0300

    feat: :sparkles: Add Cep component

---
+++ src/Cep.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Cep() {
+  return (
+    <div className="container">
+      <h1 className="title">Busca CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite o seu CEP" />
+      </div>
+    </div>
+  );
+}
+export default Cep;
