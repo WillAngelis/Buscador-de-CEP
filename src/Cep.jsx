b936488d470395a598147c2eec26d46c66fe2273
William Angelis <willangelis@gmail.com>
2022-01-10 01:19:37 -0300

    feat: :sparkles: Add section to show Cep and information

--- src/Cep.jsx
+++ src/Cep.jsx
@@ -7,9 +7,18 @@ function Cep() {
       <h1 className="title">Busca CEP</h1>
       <div className="containerInput">
         <input type="text" placeholder="Digite o seu CEP" />
-        <button className="btnSearch" >
+        <button className="btnSearch">
           <FiSearch className="animation" size={25} color="#fff" />
         </button>
+        <main className="main">
+          <h2>CEP : 12832</h2>
+          <span>Rua</span>
+          <span>Complemento : </span>
+          <span>Bairro</span>
+          <span>
+            Brasil - SP
+          </span>
+        </main> 
       </div>
     </div>
   );
