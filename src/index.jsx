61ca53b46371627d9a9b26cd169ae7adb4bdfa7d
William Angelis <willangelis@gmail.com>
2022-01-10 01:04:35 -0300

    feat: :sparkles: Add index for render page

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cep from './Cep';
+import './reset.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cep />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
